0c54762389dd8c5a219d8395bbb331727f435b6f
unknown <christianagot11@gmail.com>
2025-12-26 11:20:21 +0800

    changed header design & layout

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,30 +4,30 @@ export default function Header() {
   const [show, setShow] = useState(false);
 
   return (
-    <header className="fixed w-full px-8 py-2 min-h-12 bg-dark-2">
-      <div className="flex items-center justify-between">
-        <a className="text-white font-open-sans text-xl">
+    <>
+      <header className="fixed top-0 left-0 w-full min-h-12 px-8 py-2 flex justify-between items-center bg-secondary-1 shadow-lg">
+        <a className="font-nunito text-xl">
           gabb.
         </a>
         <nav className="hidden mt-4 flex gap-4">
-          <a className="text-white font-open-sans">About</a>
-          <a className="text-white font-open-sans">Skills</a>
-          <a className="text-white font-open-sans">Projects</a>
-          <a className="text-white font-open-sans">Contact</a>
+          <a className="font-nunito">About</a>
+          <a className="font-nunito">Skills</a>
+          <a className="font-nunito">Projects</a>
+          <a className="font-nunito">Contact</a>
         </nav>
-        <button className="cursor-pointer p-2 w-10 h-10 text-white text-2xl flex justify-center items-center hover:text-primary-1 active:text-white transition duration-200"
+        <button className="cursor-pointer p-2 w-10 h-10 text-2xl flex justify-center items-center transition duration-200"
                 onClick={() => setShow(!show)}>
           {show ? "✕" : "☰"}
         </button>
-      </div>
-      <div className={`${show ? "h-48" : "h-0"} w-full transition-[height] duration-200 overflow-hidden`}>
-        <nav className="mt-8 flex flex-col gap-4">
-          <a className="text-white font-open-sans">About</a>
-          <a className="text-white font-open-sans">Skills</a>
-          <a className="text-white font-open-sans">Projects</a>
-          <a className="text-white font-open-sans">Contact</a>
+      </header>
+      <div className="fixed top-20 w-1/2">
+        <nav className="flex flex-col gap-3">
+          <a className={`py-4 text-center font-nunito bg-secondary-1 transform ${show ? "translate-x-0" : "-translate-x-1/1"} transition-[translate] duration-350}`}>About</a>
+          <a className={`py-4 text-center font-nunito bg-secondary-1 transform ${show ? "translate-x-0" : "-translate-x-1/1"} transition-[translate] duration-350} delay-50`}>Skills</a>
+          <a className={`py-4 text-center font-nunito bg-secondary-1 transform ${show ? "translate-x-0" : "-translate-x-1/1"} transition-[translate] duration-350} delay-100`}>Projects</a>
+          <a className={`py-4 text-center font-nunito bg-secondary-1 transform ${show ? "translate-x-0" : "-translate-x-1/1"} transition-[translate] duration-350} delay-150`}>Contact</a>
         </nav>
       </div>
-    </header>
+    </>
   );
 }
