8ba4743531809e929cd626c2068fb96dc6c3a6bf
unknown <christianagot11@gmail.com>
2025-12-28 12:54:41 +0800

    trying to position lanyard to right side

--- src/sections/hero/Hero.jsx
+++ src/sections/hero/Hero.jsx
@@ -4,15 +4,8 @@ import Lanyard from "../../components/Lanyard.jsx";
 export default function Hero() {
   return (
     <>
-      <Lanyard position={[0, 0, 20]} gravity={[0, -40, 0]} />
-      <div className="size-full mt-20 px-6 flex justify-center items-center gap-6">
-        <img src={mainProfile} className="w-35"/>
-        <div className="">
-          <h1 className="mb-3 font-nunito text-3xl">Hello, I'm Gabb</h1>
-          <span>
-          An aspiring developer and tech enthusiast!
-        </span>
-        </div>
+      <div className="w-[150%] -translate-y-20">
+        <Lanyard position={[0, 0, 12]} gravity={[0, -40, 0]} />
       </div>
     </>
   );
