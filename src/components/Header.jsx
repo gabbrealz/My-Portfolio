4df44491be7da0adee13af900e31c16ac50a7f5b
unknown <christianagot11@gmail.com>
2025-12-29 04:00:11 +0800

    refactored utility classes for better readability

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
 
   return (
     <>
-      <header className="z-50 fixed top-0 left-0 w-full h-14 md:h-16 px-[7.5%] flex justify-between items-center bg-secondary-1 shadow-lg">
-        <a className="font-nunito text-md sm:text-lg md:text-xl whitespace-nowrap">
+      <header className="z-50 fixed top-0 left-0 w-full h-14 px-[7.5%] flex justify-between items-center bg-secondary-1 shadow-lg md:h-16">
+        <a className="font-nunito text-md whitespace-nowrap sm:text-lg md:text-xl">
           Christian Agot
         </a>
         <nav className="hidden md:flex md:w-full md:justify-center md:items-center md:gap-x-14 xl:gap-x-16">
@@ -15,17 +15,37 @@ export default function Header() {
           <a className="font-nunito">Projects</a>
           <a className="font-nunito">Contact</a>
         </nav>
-        <button className="md:hidden cursor-pointer p-2 w-10 h-10 text-2xl flex justify-center items-center transition duration-200"
+        <button className="cursor-pointer p-2 w-10 h-10 text-2xl flex justify-center items-center transition duration-200 md:hidden"
                 onClick={() => setShow(!show)}>
           {show ? "✕" : "☰"}
         </button>
       </header>
-      <div className="md:hidden z-50 fixed top-20 w-1/2 sm:w-1/3">
+      <div className="z-50 fixed top-20 w-1/2 sm:w-1/3 md:hidden">
         <nav className="flex flex-col gap-3">
-          <a className={`py-1 text-center font-nunito bg-secondary-1 rounded-r-lg ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350}`}>About</a>
-          <a className={`py-1 text-center font-nunito bg-secondary-1 rounded-r-lg ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350} delay-50`}>Skills</a>
-          <a className={`py-1 text-center font-nunito bg-secondary-1 rounded-r-lg ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350} delay-80`}>Projects</a>
-          <a className={`py-1 text-center font-nunito bg-secondary-1 rounded-r-lg ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350} delay-100`}>Contact</a>
+          <a className={`
+            py-1 text-center font-nunito bg-secondary-1 rounded-r-lg 
+            ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350
+          `}>
+            About
+          </a>
+          <a className={`
+            py-1 text-center font-nunito bg-secondary-1 rounded-r-lg 
+            ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350 delay-50
+          `}>
+            Skills
+          </a>
+          <a className={`
+            py-1 text-center font-nunito bg-secondary-1 rounded-r-lg 
+            ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350 delay-80
+          `}>
+            Projects
+          </a>
+          <a className={`
+            py-1 text-center font-nunito bg-secondary-1 rounded-r-lg 
+            ${show ? "translate-x-0" : "-translate-x-1/1"} transition-transform duration-350 delay-100
+          `}>
+            Contact
+          </a>
         </nav>
       </div>
     </>
--- src/sections/home/Hero.jsx
+++ src/sections/home/Hero.jsx
@@ -7,35 +7,69 @@ export default function Hero() {
   return (
     <section id="hero" className="w-screen h-screen">
       <div className="z-1 absolute w-full h-fit overflow-hidden -translate-y-20">
-        <div className="lg:w-[150vw] h-fit">
+        <div className="h-fit lg:w-[140vw] lg:scale-110">
           <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
         </div>
       </div>
 
       <div className="w-full h-full flex justify-center lg:items-center">
-        <div className="mt-[420px] md:mt-[650px] lg:mt-0 lg:pl-10 xl:pl-32 w-3/4 lg:w-[55%] flex flex-col">
-          <h1 className="font-nunito font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl text-white text-center lg:text-left leading-[1.4]">
+        <div className="
+          mt-[410px] w-3/4 flex flex-col
+          md:mt-[620px]
+          lg:mt-0 lg:pl-10 lg:w-[55%]
+          xl:pl-32
+        ">
+          <h1 className="
+            font-nunito font-bold text-3xl text-white text-center leading-[1.4]
+            sm:text-4xl
+            md:text-5xl
+            lg:text-6xl lg:text-left
+          ">
             Hey there! I'm <br/>
             <span>
               Christian Agot
             </span>
           </h1>
-          <span className="mt-3 lg:mt-4 font-nunito text-lg sm:text-xl lg:text-3xl text-center lg:text-left leading-[1.3] lg:leading-[1.5]">
+          <span className="
+            mt-3 font-nunito text-lg text-center leading-[1.3]
+            sm:text-xl
+            lg:mt-4 lg:text-3xl lg:text-left lg:leading-[1.5]
+          ">
             A passionate Computer Science student and aspiring developer.
           </span>
-          <div className="w-full mt-6 sm:mt-8 md:mt-10 lg:mt-12 flex justify-center lg:justify-start items-center gap-3 md:gap-4 lg:gap-5">
-            <a className="z-10 p-2.5 md:p-3 border md:border-2 border-white rounded-lg sm:rounded-xl">
+          <div className="
+            w-full mt-6 flex justify-center items-center gap-3
+            sm:mt-8
+            md:mt-10 md:gap-4
+            lg:mt-12 lg:justify-start lg:gap-5
+          ">
+            <a className="
+              z-10 p-2.5 border border-white rounded-lg
+              sm:rounded-xl
+              md:p-3
+              md:border-2
+            ">
               <GitHubIcon className="w-5 h-5 fill-white" />
             </a>
-            <a className="z-10 p-2.5 md:p-3 border md:border-2 border-white rounded-lg sm:rounded-xl">
+            <a className="
+              z-10 p-2.5 border border-white rounded-lg
+              sm:rounded-xl
+              md:p-3
+              md:border-2
+            ">
               <LinkedInIcon className="w-5 h-5 fill-white" />
             </a>
-            <a className="z-10 p-2.5 md:p-3 border md:border-2 border-white rounded-lg sm:rounded-xl">
+            <a className="
+              z-10 p-2.5 border border-white rounded-lg
+              sm:rounded-xl
+              md:p-3
+              md:border-2
+            ">
               <InstagramIcon className="w-5 h-5 fill-white" />
             </a>
           </div>
         </div>
-        <div className="hidden lg:block lg:w-[45%] h-1/3"></div>
+        <div className="hidden lg:block lg:w-[45%]"></div>
       </div>
     </section>
   );
