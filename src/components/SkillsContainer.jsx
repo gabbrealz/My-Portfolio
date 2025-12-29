a7130b439abfced5ec7a17103d65cfe149affb8d
unknown <christianagot11@gmail.com>
2025-12-30 06:33:39 +0800

    fixed issue with skills container grid columns not working

--- src/components/SkillsContainer.jsx
+++ src/components/SkillsContainer.jsx
@@ -1,10 +1,15 @@
 import { Children } from "react";
 
+const gridColsMap = {
+  2: "grid-cols-2",
+  3: "grid-cols-3"
+};
+
 export default function SkillsContainer({ gridColumns = 2, children }) {
   const parts = Children.toArray(children);
 
   return (
-    <div className={`relative px-2 md:px-4 py-6 grid grid-cols-${gridColumns} place-content-around gap-1 grow border rounded-lg`}>
+    <div className={`relative px-2 md:px-4 py-6 grid ${gridColsMap[gridColumns]} place-content-around gap-1 grow border rounded-lg`}>
       <span className="px-2 md:px-4 absolute top-0 left-0 -translate-y-1/2 translate-x-4 bg-secondary-2 font-nunito font-bold text-sm md:text-md lg:text-lg xl:text-xl">
         {parts[0]}
       </span>
