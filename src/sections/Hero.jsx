9dc39f5d46473c6fb88e0f5248584cc78fa4a04c
unknown <christianagot11@gmail.com>
2025-12-29 17:59:24 +0800

    fixed more of the hero layout

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,22 +5,21 @@ import InstagramIcon from "../assets/svg/instagram.svg?react";
 
 export default function Hero() {
   return (
-    <section id="hero" className="w-screen h-screen overflow-hidden">
+    <section id="hero" className="w-screen min-h-screen h-auto lg:h-screen">
       <div className="z-1 absolute w-full h-fit overflow-hidden -translate-y-20">
-        <div className="h-fit lg:w-[140vw] lg:scale-110">
+        <div className="h-fit lg:w-[130vw] lg:scale-110">
           <Lanyard position={[0, 0, 15]} gravity={[0, -40, 0]} />
         </div>
       </div>
 
       <div className="w-full h-full flex justify-center lg:items-center">
         <div className="
-          mt-[410px] w-3/4 flex flex-col
-          md:mt-[620px]
+          mt-[60vh] w-3/4 flex flex-col
           lg:mt-0 lg:pl-10 lg:w-[55%]
           xl:pl-32
         ">
           <h1 className="
-            font-nunito font-bold text-3xl text-white text-center leading-[1.4]
+            z-10 font-nunito font-bold text-3xl text-white text-center leading-[1.4]
             sm:text-4xl
             md:text-5xl
             lg:text-6xl lg:text-left
@@ -31,15 +30,21 @@ export default function Hero() {
             </span>
           </h1>
           <span className="
-            mt-2 font-nunito text-lg text-center leading-[1.3]
-            sm:text-xl
-            lg:mt-4 lg:text-3xl lg:text-left lg:leading-[1.5]
+            z-10 mt-4 font-nunito text-xl text-center leading-[1.3]
+            sm:text-2xl
+            lg:text-3xl lg:text-left lg:leading-[1.5]
           ">
-            A passionate Computer Science student and aspiring developer.
+            A passionate{" "}
+            <span className="whitespace-nowrap">
+              CS student{" "}
+            </span>
+            and aspiring{" "}
+            <span className="whitespace-nowrap">
+              Software Engineer.
+            </span>
           </span>
           <div className="
-            w-full mt-6 flex justify-center items-center gap-3
-            sm:mt-8
+            w-full mt-8 flex justify-center items-center gap-3
             md:mt-10 md:gap-4
             lg:mt-12 lg:justify-start lg:gap-5
           ">
