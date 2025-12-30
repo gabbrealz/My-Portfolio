54a02739a13b00427c9bce079a14962e75776863
unknown <christianagot11@gmail.com>
2025-12-30 22:28:21 +0800

    started with project section (heading is done)

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,5 +1,25 @@
 export default function Projects() {
   return (
-    <></>
+    <section id="projects" className="
+      mt-24 mx-auto px-8
+      sm:mt-30
+      md:mt-36 md:w-[85%] md:px-0
+      lg:mt-42 lg:w-[80%]
+      xl:mt-48 xl:w-[70%]
+    ">
+      <div className="
+        w-full mb-8 flex justify-center items-center gap-4
+        sm:mb-10 sm:gap-8
+        md:mb-12
+        lg:mb-14
+        xl:mb-16
+      ">
+        <h2>PROJECTS</h2>
+        <span className="w-full h-0.25 bg-highlight-1"></span>
+      </div>
+      <div className="flex flex-col gap-2">
+
+      </div>
+    </section>
   );
 }
