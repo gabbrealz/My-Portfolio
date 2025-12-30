b19425e6b8c5ee93f3b0adec5a9b36ce9c73eef9
unknown <christianagot11@gmail.com>
2025-12-30 22:24:14 +0800

    minor changes to spacing

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -27,9 +27,9 @@ export default function Hero() {
             </span>
           </h1>
           <span className="
-            z-10 mt-4 font-body text-lg text-center leading-[1.3]
+            z-10 mt-6 font-body text-lg text-center leading-[1.3]
             sm:text-xl md:text-2xl
-            lg:text-left lg:mt-6 lg:leading-[1.5]
+            lg:text-left lg:leading-[1.5]
             xl:text-3xl
           ">
             A passionate{" "}
