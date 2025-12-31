20f6facd9e0103c1cfb874bad929bed045f9b81d
unknown <christianagot11@gmail.com>
2025-12-31 18:32:15 +0800

    refactored svg's (put them in different directories)

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import Lanyard from "../components/Lanyard.jsx";
-import GitHubIcon from "../assets/svg/github.svg?react";
-import LinkedInIcon from "../assets/svg/linkedin.svg?react";
-import InstagramIcon from "../assets/svg/instagram.svg?react";
+import GitHubIcon from "../assets/svg/icons/github.svg?react";
+import LinkedInIcon from "../assets/svg/icons/linkedin.svg?react";
+import InstagramIcon from "../assets/svg/icons/instagram.svg?react";
 
 export default function Hero() {
   return (
