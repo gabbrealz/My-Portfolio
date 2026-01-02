62968e43dba9f0b2e6f5b362725a4a62424ebb98
unknown <christianagot11@gmail.com>
2026-01-02 13:15:21 +0800

    added mailto button to hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,7 @@
 import Lanyard from "../components/Lanyard.jsx";
+import MailIcon from "../assets/svg/icons/envelope.svg?react";
 import GitHubIcon from "../assets/svg/icons/github.svg?react";
 import LinkedInIcon from "../assets/svg/icons/linkedin.svg?react";
-import InstagramIcon from "../assets/svg/icons/instagram.svg?react";
 
 export default function Hero() {
   return (
@@ -27,7 +27,7 @@ export default function Hero() {
             </span>
           </h1>
           <span className="
-            z-10 mt-6 font-body text-lg text-center leading-[1.3] text-shadow-md text-shadow-black
+            z-10 mt-4 font-body text-lg text-center leading-[1.3] text-shadow-md text-shadow-black
             sm:text-xl md:text-2xl
             lg:text-left lg:leading-[1.5]
             xl:text-3xl
@@ -42,20 +42,24 @@ export default function Hero() {
             </span>
           </span>
           <div className="
-            w-full mt-8 flex justify-center items-center gap-3
-            md:mt-10 md:gap-4
-            lg:mt-12 lg:justify-start lg:gap-5
-            xl:mt-14 xl:gap-6
+            mt-6 w-full flex flex-wrap justify-center gap-4
+            md:mt-8 md:gap-8
+            lg:mt-10 lg:justify-start lg:gap-10
+            xl:mt-12 xl:gap-12
           ">
-            <a className="z-10 p-2.5 border rounded-xl md:p-3 md:border-2">
-              <GitHubIcon className="w-5 h-5 sm:w-6 sm:h-6" />
+            <a className="z-10 px-3 border rounded-full font-body flex justify-center items-center sm:px-4 md:px-5">
+              <MailIcon className="size-4 sm:size-4.5" />
+              <span className="text-sm ml-2 sm:ml-2.5 sm:text-md md:text-lg">Get In Touch</span>
             </a>
-            <a className="z-10 p-2.5 border rounded-xl md:p-3 md:border-2">
-              <LinkedInIcon className="w-5 h-5 sm:w-6 sm:h-6" />
-            </a>
-            <a className="z-10 p-2.5 border rounded-xl md:p-3 md:border-2">
-              <InstagramIcon className="w-5 h-5 sm:w-6 sm:h-6" />
-            </a>
+            <span className="hidden w-0.25 bg-white rounded-full sm:inline"></span>
+            <div className="flex justify-center items-center gap-2 md:gap-2.5 lg:gap-3 xl:gap-3.5">
+              <a className="z-10 p-2.5 border rounded-xl md:p-3">
+                <GitHubIcon className="size-5 sm:size-6" />
+              </a>
+              <a className="z-10 p-2.5 border rounded-xl md:p-3">
+                <LinkedInIcon className="size-5 sm:size-6" />
+              </a>
+            </div>
           </div>
         </div>
         <div className="hidden lg:block lg:w-[45%]"></div>
