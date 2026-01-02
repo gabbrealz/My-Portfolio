3b4551706dc1e77a6af0e13eedfcac4a67d68ff4
unknown <christianagot11@gmail.com>
2026-01-02 14:06:37 +0800

    removed line elements from SectionHeading component also moved its font size styling from styles.css to within the component

--- src/components/SectionHeading.jsx
+++ src/components/SectionHeading.jsx
@@ -1,13 +1,9 @@
 export default function SectionHeading({ heading = "left", className, children }) {
-  return (
-    <div className={`
-      w-full flex items-center gap-4 sm:gap-8
-      ${heading === "right" ? "justify-end" : heading === "center" ? "sm:justify-center" : "" }
+  return <h2 className={`
+      whitespace-nowrap w-full text-xl md:text-2xl lg:text-3xl
+      ${heading === "center" ? "sm:text-center" : heading === "right" ? "text-right" : ""}
       ${className}
     `}>
-      <span className={`w-1/2 h-0.25 bg-highlight-1 ${heading === "right" ? "" : heading === "center" ? "hidden sm:inline sm:w-1/4" : "hidden"}`}></span>
-      <h2 className="whitespace-nowrap">{children}</h2>
-      <span className={`w-1/2 h-0.25 bg-highlight-1 ${heading === "right" ? "hidden" : heading === "center" ? "sm:w-1/4" : ""}`}></span>
-    </div>
-  );
+      {children}
+    </h2>;
 }
