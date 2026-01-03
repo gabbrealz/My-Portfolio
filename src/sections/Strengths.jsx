d52e29d5ab9d489bd4fff650363e0251f8ce3e29
unknown <christianagot11@gmail.com>
2026-01-04 00:02:56 +0800

    changed strengths section styling from having each title in container border to having them within the container

--- src/sections/Strengths.jsx
+++ src/sections/Strengths.jsx
@@ -1,5 +1,5 @@
 import SectionHeading from "../components/SectionHeading.jsx";
-import { BorderTitleContainer } from "../components/Container.jsx";
+import { Container } from "../components/Container.jsx";
 
 import codeImgSrc from "../assets/images/strengths/code.jpg";
 import planImgSrc from "../assets/images/strengths/plan.jpg";
@@ -12,45 +12,30 @@ export default function Strengths({ ref }) {
         <>These are my strengths in software development.</>
       </SectionHeading>
       <div className="flex flex-wrap justify-center gap-8 md:gap-2">
-        <BorderTitleContainer className="group rounded-none hover:-translate-x-2 transition-transform duration-150 md:w-[49%]"
-                              titleAlign="center"
-                              titleClassName="z-10 whitespace-nowrap text-center text-sm sm:text-md lg:text-xl xl:text-2xl
-        ">
-          <>BACKEND DEVELOPMENT</>
-          <>
-            <img src={codeImgSrc} alt="Java Code" className="aspect-5/3 object-cover w-full opacity-20 group-hover:opacity-30 transition-opacity duration-250"/>
-            <p className="
-              text-xs p-12 absolute top-0 left-0 size-full flex justify-center items-center font-body text-center text-shadow-lg text-shadow-black
-              sm:text-sm
-              md:p-4
-              lg:p-12 lg:text-base
-              xl:leading-[2]
-            ">
+        <Container className="group rounded-none hover:-translate-x-2 transition-transform duration-150 md:w-[49%]">
+          <img src={codeImgSrc} alt="Java Code" className="aspect-5/3 object-cover w-full opacity-20 group-hover:opacity-30 transition-opacity duration-150"/>
+          <div className="h-full absolute top-0 left-0 flex flex-col justify-center items-center p-4 gap-2 sm:p-24 md:gap-4 md:p-4 lg:gap-6 lg:p-12">
+            <h3 className="font-heading text-center text-highlight-2 text-shadow-lg text-shadow-black text-sm sm:text-lg md:text-base lg:text-xl">
+              BACKEND DEVELOPMENT
+            </h3>
+            <p className="font-body text-center text-shadow-lg text-shadow-black text-xs sm:text-sm lg:text-base xl:text-lg xl:leading-[2]">
               I enjoy building robust backend systems using clean, maintainable code.
-              Through projects, I have developed a good understanding of core backend principles and best practices.
+              I also have a core foundation in algorithmic thinking!
             </p>
-          </>
-        </BorderTitleContainer>
-        <BorderTitleContainer className="group rounded-none hover:translate-x-2 transition-transform duration-150 md:w-[49%]"
-                              titleAlign="center"
-                              titleClassName="z-10 whitespace-nowrap text-center text-sm sm:text-md lg:text-xl xl:text-2xl
-        ">
-          <>ARCHITECTURE PLANNING</>
-          <>
-            <img src={planImgSrc} alt="Notes on Data Structures" className="aspect-5/3 object-cover w-full opacity-20 group-hover:opacity-30 transition-opacity duration-250"/>
-            <p className="
-              text-xs p-12 absolute top-0 left-0 size-full flex justify-center items-center font-body text-center text-shadow-lg text-shadow-black
-              sm:text-sm
-              md:p-4
-              lg:p-12 lg:text-base
-              xl:leading-[2]
-            ">
-              I am great at planning efficient and scalable software architecture. I enjoy breaking
-              down complex systems into manageable components, and structuring projects in a way that makes future
-              management easier.
+          </div>
+        </Container>
+        <Container className="group rounded-none hover:translate-x-2 transition-transform duration-150 md:w-[49%]">
+          <img src={planImgSrc} alt="Notes on Data Structures" className="aspect-5/3 object-cover w-full opacity-20 group-hover:opacity-30 transition-opacity duration-150"/>
+          <div className="h-full absolute top-0 left-0 flex flex-col justify-center items-center p-4 gap-2 sm:p-24 md:gap-4 md:p-4 lg:gap-6 lg:p-12">
+            <h3 className="font-heading text-center text-highlight-2 text-shadow-lg text-shadow-black text-sm sm:text-lg md:text-base lg:text-xl">
+              ARCHITECTURE PLANNING
+            </h3>
+            <p className="font-body text-center text-shadow-lg text-shadow-black text-xs sm:text-sm lg:text-base xl:text-lg xl:leading-[2]">
+              I enjoy breaking down complex systems into manageable components, and structuring
+              projects in a way that makes future management easier.
             </p>
-          </>
-        </BorderTitleContainer>
+          </div>
+        </Container>
       </div>
     </section>
   );
